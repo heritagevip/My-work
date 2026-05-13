fdde042e429de55a7d8fc36ac46fde5bfb281884
v.i.p <ademokunwadaniel@gmail.com>
2026-05-13 12:45:10 -0700

    addded a student list

--- test.py
+++ test.py
@@ -2,15 +2,17 @@ school_name = "FUTES"
 school_address = "IYIN EKITI"
 course_studing = "Software Engineering"
 student_name = "HERITAGE"
+students = ["heritage", "john", "michael", "susan"]
 student_age = "6 7"
 total_fees = (150000)
-amount_paid = (110000)
+amount_paid = (10000)
 has_scolarship = (False)
-student_remark = ("Heritage is a good boy")
+student_remark = (" is a good boy")
 balance = total_fees - amount_paid
-if balance == 0: 
-  print("Status Cleared")
-elif balance<50000:
-  print("status; Pay us as soon as possible")
-else: 
-  print("Status; Denied")
+for name in students:
+  if balance == 0:
+    print(f"status you are cleared for the semester {name} {student_remark}")
+  elif balance <= 50000:
+     print(f"{name} im giving you last warning before i wound you ")
+  else:
+    print(f"{name} go to your father house ")
