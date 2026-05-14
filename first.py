d6ea9486ca5184864e085f704d40a1d62612c499
v.i.p <ademokunwadaniel@gmail.com>
2026-05-14 08:09:12 -0700

    loops

--- first.py
+++ first.py
@@ -1,4 +1,14 @@
-total_fees = (150000)
-amount_paid = (90000)
-has_scolarship = (False)
-student_remark = ("Heritage is a good boy")
+school_fees = 150000
+student_data = {
+    "heritage is late ": 100000,
+    "john is not late": 15000,
+    "victor is late": 70000,
+    "daniel is not late": 90000,
+    "soj is late":120000,
+}
+for name, paid in student_data.items():
+    balance = school_fees - paid 
+if "late" in name:
+    print(f"{name}{balance + 5000}")
+else:
+    print(f"{name} cleared")
--- test.py
+++ test.py
@@ -3,16 +3,18 @@ school_address = "IYIN EKITI"
 course_studing = "Software Engineering"
 student_name = "HERITAGE"
 students = ["heritage", "john", "michael", "susan"]
-student_age = "6 7"
+student_age = ["17", "18", "19", "20"]
 total_fees = (150000)
 amount_paid = (10000)
 has_scolarship = (False)
 student_remark = (" is a good boy")
 balance = total_fees - amount_paid
-for name in students:
-  if balance == 0:
-    print(f"status you are cleared for the semester {name} {student_remark}")
-  elif balance <= 50000:
-     print(f"{name} im giving you last warning before i wound you ")
-  else:
-    print(f"{name} go to your father house ")
+student_data = {
+  "heritage": 150000,
+  "john": 0,
+  "micheal": 90000,
+  "susan": 120000,
+}
+for name, paid in student_data.items():
+  balance = total_fees - paid
+  print(f"{name} paid {paid} and your balance is {balance}")
